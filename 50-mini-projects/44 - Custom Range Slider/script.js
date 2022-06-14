606a052a3933f53cbdc5706f523b52f435953410
Manoj Kumar <manojkumar.foo@gmail.com>
2022-06-15 02:19:03 +0530

    Attribute updates

--- 50-mini-projects/44 - Custom Range Slider/script.js
+++ 50-mini-projects/44 - Custom Range Slider/script.js
@@ -1,11 +1,15 @@
 const inputRange = document.querySelector("input[type='range']");
 const rangeNumberEl = document.querySelector(".range-number");
+
+console.log(inputRange.offsetWidth);
+console.log(inputRange.max);
 inputRange.addEventListener("input", (e) => {
   rangeNumberEl.innerText = e.target.value;
-  console.log(inputRange.offsetWidth);
-  rangeNumberEl.style.left = `${e.target.value * 2.7}px`;
-  console.log(e.target.value);
+  let width =
+    (e.target.value * inputRange.offsetWidth) / 100 -
+    rangeNumberEl.offsetWidth / 2;
+  rangeNumberEl.style.left = `${width}px`;
 });
 
 rangeNumberEl.innerText = inputRange.value;
-rangeNumberEl.style.left = `${inputRange.value * 2.7}px`;
+rangeNumberEl.style.left = `${inputRange.value * 2.6}px`;
