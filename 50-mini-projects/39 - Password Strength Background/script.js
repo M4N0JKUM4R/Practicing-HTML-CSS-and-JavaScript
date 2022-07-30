176e5f7c767127f4985030b999997ca213ef4ebf
Manoj Kumar <manojkumar.foo@gmail.com>
2022-07-30 15:38:47 +0530

    Password Strength Background done.

---
+++ 50-mini-projects/39 - Password Strength Background/script.js
@@ -0,0 +1,36 @@
+let passwordEl = document.querySelector(".password");
+let containerEl = document.querySelector(".container");
+
+passwordEl.addEventListener("input", (e) => {
+  passScore(e.target.value);
+});
+
+function passScore(value) {
+  let score = 0;
+  const valueArray = value.split("");
+
+  for (let i = 0; i < valueArray.length; i++) {
+    score += 5;
+  }
+
+  let variations = {
+    digit: /\d/.test(value),
+    lower: /[a-z]/.test(value),
+    upper: /[A-Z]/.test(value),
+    nonWord: /\W/.test(value),
+  };
+
+  let variationCount = 0;
+
+  for (let check in variations) {
+    variationCount += variations[check] === true ? 1 : 0;
+  }
+
+  score += variationCount * 10;
+  console.log("Variation Count", variationCount);
+  console.log("Score", score);
+
+  let ratio = 100;
+  let blurValue = ratio / score;
+  containerEl.style.filter = `blur(${blurValue < 1 ? 0 : blurValue}px)`;
+}
